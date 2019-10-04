ea0aa1fa3de9ad31bc9fba0242c4da56271cc70c
Luke <lukekrete@gmail.com>
2019-10-04 09:18:24 -0400

    Simple twitter scraper

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Python_Web_Scraper
  A web scraper coded in Python
+
+Required libraries: Beautiful Soup and Requests 
+
+Running in a Venv:
+
+	1. Go to Terminal and type: python -m venv tutorial-env
+	2. Next in Terminal, type: tutorial-env\Scripts\activate.bat
+	3. Make sure to then pip install the 2 libraries: bs4 and requests
---
+++ webscraper.py
@@ -0,0 +1,23 @@
+#-------------------------------
+# Date: October 4th, 2019
+# Author: Luke Krete
+# Description:  Currently set up
+#				scrape a twitter
+#				page
+#-------------------------------
+
+#------Imports------------------
+from bs4 import BeautifulSoup
+import requests
+#-------------------------------
+
+all_tweets = []
+url = 'https://twitter.com/TheOnion'
+response = requests.get(url)
+content = BeautifulSoup(response.text, "html.parser")
+timeline = html.select('#timeline li.stream-item')
+for tweet in timeline:
+	tweet_id = tweet['data-item-id']
+	tweet_text = tweet.select('p.tweet-text')[0].get_text()
+	all_tweets.append({"id": tweet_id, "text": teet_text})
+	print(all_tweets)
